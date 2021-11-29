7c58cc901c315446d384f04510b88d16c4fb70de
Patrick He <77468352+phetheep@users.noreply.github.com>
2021-11-29 11:13:55 -0500

    PHP code

--- Login/loginHandler.php
+++ Login/loginHandler.php
@@ -1,5 +1,26 @@
 <?php
-    echo $_POST['username'];
-    echo $_POST['password'];
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "loginDB";
+$uname = $_GET["username"];
+$pcode = $_GET["passcode"];
 
+// Create connection
+$conn = new mysqli($servername, $username, $passcode, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO loginInfo (username, passcode)
+VALUES ($uname, $pcode)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
