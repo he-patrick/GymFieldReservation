8c48ff282786d8268412d655a0ac22fbe769a8a6
Patrick He <77468352+phetheep@users.noreply.github.com>
2021-11-30 11:14:37 -0500

    Login

--- Login/loginHandler.php
+++ Login/loginHandler.php
@@ -1,15 +1,38 @@
 <?php
-if(isset($_POST['username'])){
-    $uname = $_POST['username'];
+/*
+session_start();
+$DATABASE_HOST = "localhost";
+$DATABASE_USER = "root";
+$DATABASE_PASS = "";
+$DATABASE_NAME = "loginDB";
+
+// Create connection
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PAS, $DATABASE_NAME);
+// Check connection
+if (mysqli_connect_errno()) {
+  exit("Connection failed: " . mysqli_connect_error());
 }
-if(isset($_POST['password'])){
-    $pword = $_POST['password'];
-}
-if (strcmp($uname, 'megan.port@ocdsb.ca') !== 0 || strcmp($pword, 'earlathletics') !== 0) {
-	// Correct username and password, logged in	
-  echo 'Incorrect username and/or password!';
+
+$sql = "INSERT INTO loginInfo (username, passcode)
+VALUES ($uname, $pcode)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-    // Incorrect password
-    echo 'welcome!';
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
+*/
+
+$u = 'megan.port@ocdsb.ca';
+$p = 'earlathletics';
+if ($_POST['username'] == $u && $_POST['passcode'] == $p) {
+	// Correct username and password, logged in	
+		echo 'Welcome';
+} else {
+	// Incorrect password
+  echo 'Incorrect username and/or password!';
+}
+
+//$conn->close();
+
 ?>
