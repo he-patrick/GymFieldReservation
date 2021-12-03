4ce1e94a9df07f114472eed8dd3a1ce227dff45c
Patrick He <77468352+phetheep@users.noreply.github.com>
2021-12-03 09:10:47 -0500

    DB

--- Login/Home Page/Reservation Pages/reservationDB.sql
+++ Login/Home Page/Reservation Pages/reservationDB.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE reservationDB;
 
 CREATE TABLE Users {
+    Reservation     NVARCHAR(255),
+    PersonID        NVARCHAR(255),
     ReservationDate NVARCHAR(255),
     ReservationTime NVARCHAR(255),
     FirstName       NVARCHAR(255),
