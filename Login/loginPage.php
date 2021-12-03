029f22d178614bb9d78cba38b8db0e3f9758d544
CalvinCalluo <calvin.d.luo@gmail.com>
2021-12-03 09:25:30 -0500

    Update loginPage.php

--- Login/loginPage.php
+++ Login/loginPage.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 ?>
-+
+
 <!DOCTYPE html> 
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <style>
@@ -29,14 +29,20 @@ session_start();
                     <input class="text-field" type="text" id="username" name="username" placeholder="Username or Email"><br><br>
                     <input class="text-field" type="password" id="passcode" name="passcode" placeholder="Password"><br><br>
                     <input class="w3-cyan w3-hover-light-green w3-border-0 text-field" type="submit" value="Log in">
-                        <?php
-                            if(isset($_SESSION["error"])){
-                                $error = $_SESSION["error"];
-                                echo "<span>$error</span>";
-                            }
-                        ?>  
-                    </form>
+                    <?php
+                        if(isset($_SESSION["error"])){
+                            $error = $_SESSION["error"];
+                        }
+                    ?>
+                </form>
                 <br>
+                <div class="w3-padding-small w3-text-red">
+                <?php
+                    if(isset($error)){
+                        echo $error;
+                    }   
+                ?>
+                </div>
                 <button class="text-field w3-cyan w3-hover-light-green w3-border-0 w3-auto" onclick="document.getElementById('contactModal').style.display='block';">Contact</button>
             </div>
             <!--Modal-->
@@ -46,7 +52,7 @@ session_start();
                       <span onclick="document.getElementById('contactModal').style.display='none'" class="w3-button w3-display-topright">&times;</span>
                       <img src="Images/MsPort.png" alt="MsPort" style="width: 100%;height: 60%;">
                     </div>
-                    <div class="w3-secti5on w3-container w3-center">
+                    <div class="w3-section w3-container w3-center">
                       <h2 class="w3-wide"><strong><em>Megan Port</em></strong></h2>
                       <h4><a href="mailto: megan.port@ocdsb.ca" style="color: blue;">megan.port@ocdsb.ca</span></a></h4>
                     </div>
@@ -65,3 +71,7 @@ session_start();
         </script>
     </body>
 </html>
+
+<?php
+    unset($_SESSION["error"]);
+?>
