08b1a9f9a0d3f10bbe9836dda11d9c847db8f5fe
CalvinCalluo <calvin.d.luo@gmail.com>
2021-12-13 10:56:50 -0500

    Update reservationHandler.php

--- Login/Home Page/Reservation Pages/reservationHandler.php
+++ Login/Home Page/Reservation Pages/reservationHandler.php
@@ -7,16 +7,15 @@
     $dbname = "reservationdb";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    $result = $conn->query("SELECT id FROM reservationinfo WHERE ReservationTime = $_POST[times]");
-    if($result->num_rows == 0) {
+    $reservationDate = $_POST["reservation"];
+    $reservationTime = $_POST["times"];
+    $area = $_POST["ReservationArea"];
+    $result = $conn->query("SELECT * FROM reservationinfo WHERE ReservationTime = $reservationTime AND ReservationDate = $reservationDate AND Area = $area");
+    if($result == FALSE) {
          // row not found, do stuff...
-         $reservationDate = $_POST["reservation"];
-         $reservationTime = $_POST["times"];
          $firstName = $_POST["fname"];
          $lastName = $_POST["lname"];
          $email = $_POST["email"];
-         $area = $_POST["ReservationArea"];
          $sql = "INSERT INTO reservationinfo (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
          mysqli_query($conn, $sql) or die('Error, insert query failed');
          if($conn->query($sql) == TRUE){
@@ -26,9 +25,8 @@
     } else {
         // do other stuff...
         echo "failed";
+        $conn->close();
     }
-    $mysqli->close();
-
     
     /*
     $to      = 'phe1@ocdsb.ca';
