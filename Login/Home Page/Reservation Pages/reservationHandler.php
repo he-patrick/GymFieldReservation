fa8d5ad1d8ce3a1998420ab3f42b3e8571cc8867
CalvinCalluo <calvin.d.luo@gmail.com>
2021-12-13 09:26:22 -0500

    Put hidden form input on halfmain field and made databas ework

--- Login/Home Page/Reservation Pages/reservationHandler.php
+++ Login/Home Page/Reservation Pages/reservationHandler.php
@@ -1,12 +1,25 @@
 <?php
-    $reservationDate = $_POST["reserveDate"];
-    $reservationTime = $_POST["timeSelection"];
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "reservationdb";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    $reservationDate = $_POST["reservation"];
+    $reservationTime = $_POST["times"];
     $firstName = $_POST["fname"];
     $lastName = $_POST["lname"];
     $email = $_POST["email"];
     $area = $_POST["ReservationArea"];
-    echo $area;
-/*
+    $sql = "INSERT INTO reservationinfo (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
+    mysqli_query($conn, $sql) or die('Error, insert query failed');
+    if($conn->query($sql) == TRUE){
+        echo "Successfully Booked";
+    }
+    $conn->close();
+    /*
     $to      = 'phe1@ocdsb.ca';
     $subject = 'the subject';
     $message = 'hello';
