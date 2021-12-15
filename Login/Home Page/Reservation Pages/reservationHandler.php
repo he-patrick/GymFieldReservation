1e2ebd3438a465ce899144152fc531357a7f92ea
Patrick He <77468352+phetheep@users.noreply.github.com>
2021-12-15 09:38:54 -0500

    bi

--- Login/Home Page/Reservation Pages/reservationHandler.php
+++ Login/Home Page/Reservation Pages/reservationHandler.php
@@ -1,3 +1,4 @@
+
 <?php
   
     session_start();
@@ -10,13 +11,13 @@
     $reservationDate = $_POST["reservation"];
     $reservationTime = $_POST["times"];
     $area = $_POST["ReservationArea"];
-    $result = mysqli_query($conn, "SELECT personID FROM reservationinfo WHERE ReservationTime = '$reservationTime' AND ReservationDate = '$reservationDate' AND Area = '$area'");
-    if(mysqli_num_rows($result) == 0) {
+    $result = $conn->query("SELECT * FROM reservationdb WHERE ReservationTime = $reservationTime AND ReservationDate = $reservationDate AND Area = $area");
+    if($result == FALSE) {
          // row not found, do stuff...
          $firstName = $_POST["fname"];
          $lastName = $_POST["lname"];
          $email = $_POST["email"];
-         $sql = "INSERT INTO reservationinfo (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
+         $sql = "INSERT INTO reservationdb (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
          mysqli_query($conn, $sql) or die('Error, insert query failed');
          if($conn->query($sql) == TRUE){
              echo "Successfully Booked";
@@ -27,14 +28,36 @@
         echo "failed";
         $conn->close();
     }
-    
-    /*
-    $to      = 'phe1@ocdsb.ca';
-    $subject = 'the subject';
-    $message = 'hello';
-    $headers = 'From: phe1@ocdsb.ca'       . "\r\n" .
-                 'Reply-To: webmaster@example.com' . "\r\n" .
-                 'X-Mailer: PHP/' . phpversion();
+   
+use PHPMailer;
+use SMTP;
+use Exception;
+require 'vendor/autoload.php';
 
-    mail($to, $subject, $message, $headers);*/
+$mail = new PHPMailer(true);
+
+//Send mail using gmail
+if($send_using_gmail){
+    $mail->IsSMTP(); // telling the class to use SMTP
+    $mail->SMTPAuth = true; // enable SMTP authentication
+    $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+    $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+    $mail->Port = 465; // set the SMTP port for the GMAIL server
+    $mail->Username = "your-gmail-account@gmail.com"; // GMAIL username
+    $mail->Password = "your-gmail-password"; // GMAIL password
+}
+
+//Typical mail data
+$mail->AddAddress($email, $name);
+$mail->SetFrom($email_from, $name_from);
+$mail->Subject = "My Subject";
+$mail->Body = "Mail contents";
+
+try{
+    $mail->Send();
+    echo "Success!";
+} catch(Exception $e){
+    //Something went bad
+    echo "Fail - " . $mail->ErrorInfo;
+}
 ?>
