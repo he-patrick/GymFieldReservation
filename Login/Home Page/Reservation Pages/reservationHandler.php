4b13dafad2f8aa22704a80bd13555e722a6beb84
CalvinCalluo <calvin.d.luo@gmail.com>
2021-12-16 10:25:00 -0500

    THE BIG C AND P DO IT ONCE AGAIN. WE ARE A BIT A BIT OF A PROBLEM!!!!!

--- Login/Home Page/Reservation Pages/reservationHandler.php
+++ Login/Home Page/Reservation Pages/reservationHandler.php
@@ -1,56 +1,68 @@
 
 <?php
   
-    session_start();
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "reservationdb";
+  session_start();
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "reservationdb";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    $reservationDate = $_POST["reservation"];
-    $reservationTime = $_POST["times"];
-    $area = $_POST["ReservationArea"];
-    $result = $conn->query("SELECT * FROM reservationdb WHERE ReservationTime = $reservationTime AND ReservationDate = $reservationDate AND Area = $area");
-    if($result == FALSE) {
-         // row not found, do stuff...
-         $firstName = $_POST["fname"];
-         $lastName = $_POST["lname"];
-         $email = $_POST["email"];
-         $sql = "INSERT INTO reservationdb (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
-         mysqli_query($conn, $sql) or die('Error, insert query failed');
-         if($conn->query($sql) == TRUE){
-             echo "Successfully Booked";
-         }
-         $conn->close();
-    } else {
-        // do other stuff...
-        echo "failed";
-        $conn->close();
-    }
-   
-    require("/home/site/libs/PHPMailer-master/src/PHPMailer.php");
-    require("/home/site/libs/PHPMailer-master/src/SMTP.php");
-  
-      $mail = new PHPMailer\PHPMailer\PHPMailer();
-      $mail->IsSMTP(); // enable SMTP
-  
-      $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-      $mail->SMTPAuth = true; // authentication enabled
-      $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-      $mail->Host = "smtp.gmail.com";
-      $mail->Port = 465; // or 587
-      $mail->IsHTML(true);
-      $mail->Username = "phetheep@gmail.com";
-      $mail->Password = "Phe6596508886";
-      $mail->SetFrom("phetheep@gmail.com");
-      $mail->Subject = "Test";
-      $mail->Body = "hello";
-      $mail->AddAddress("phetheep@gmail.com");
-  
-       if(!$mail->Send()) {
-          echo "Mailer Error: " . $mail->ErrorInfo;
-       } else {
-          echo "Message has been sent";
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $reservationDate = $_POST["reservation"];
+  $reservationTime = $_POST["times"];
+  $area = $_POST["ReservationArea"];
+  $result = mysqli_query($conn, "SELECT * FROM reservationdb WHERE ReservationTime = $reservationTime AND ReservationDate = $reservationDate AND Area = $area");
+  if($result == 0) {
+       // row not found, do stuff...
+       $firstName = $_POST["fname"];
+       $lastName = $_POST["lname"];
+       $email = $_POST["email"];
+       $sql = "INSERT INTO reservationinfo (ReservationDate, ReservationTime, FirstName, LastName, Email, Area) VALUES ('$reservationDate', '$reservationTime', '$firstName', '$lastName', '$email', '$area')";
+       mysqli_query($conn, $sql) or die('Error, insert query failed');
+       if($conn->query($sql) == TRUE){
+           echo "Successfully Booked";
        }
-  ?>
+  } else {
+      // do other stuff...
+      echo "failed";
+  }
+  $conn->close();
+  
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output                                          //Send using SMTP
+    $mail->Host       = "smtp.gmail.com";                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'gymfieldbooking@gmail.com';                     //SMTP username
+    $mail->Password   = 'earlofmarchss';                               //SMTP password
+    $mail->SMTPSecure = 'tls';
+    $mail->SMTPAutoTLS = false;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('gymfieldbooking@gmail.com', 'GymFieldBooking', 0);
+    $mail->addAddress('calvin.d.luo@gmail.com', 'Calvin');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
